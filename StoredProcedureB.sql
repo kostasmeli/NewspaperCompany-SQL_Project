14e96bb0ef73a37eb79a94890c841081e3920366
kwstasmel <33500174+kwstasmel@users.noreply.github.com>
2020-09-29 19:12:33 +0300

    Stored Procedure B
    if he is not a journalist , doesnt set salary to null ,but invokes error

--- StoredProcedureB.sql
+++ StoredProcedureB.sql
@@ -8,11 +8,16 @@ DECLARE prouphresia INT;
 DECLARE hmeromhnia_proslipsis DATE;
 DECLARE misthos FLOAT;
 SET hmeromhnia=CURDATE();
+IF ((SELECT email FROM Journalist WHERE email=email_dhmosiografou) IS NULL) THEN
+SIGNAL SQLSTATE VALUE '45000'
+SET MESSAGE_TEXT="Not journalist";
+ELSE
 SELECT employment_date,working_experience_on_registration,salary INTO hmeromhnia_proslipsis,prouphresia,misthos
 FROM Employee INNER JOIN Journalist  on Employee.email=Journalist.email
 WHERE Journalist.email = email_dhmosiografou;
 SET mhnes= datediff(hmeromhnia,hmeromhnia_proslipsis)/30 + prouphresia;
 SET misthos=misthos + (misthos*0.005*mhnes);
 UPDATE Employee SET Employee.salary=misthos WHERE Employee.email=email_dhmosiografou;
+END IF;
 END$
 DELIMITER ;
